7a61f597a46f05542480d3274a4d3995b8c4af7e
lmlat <38957459+lmlat@users.noreply.github.com>
2026-02-07 00:19:14 +0800

    Update font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -10,6 +10,7 @@ module.exports = {
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
+    'https://cdn.jsdmirror.com/npm/lxgw-wenkai-webfont@1.7.0/style.min.css',
     'https://fonts.googleapis.com/css?family=Bitter:300,400,700&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;500;700&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300;400;500;700&display=swap'
@@ -21,12 +22,12 @@ module.exports = {
   FONT_SUBSET: process.env.NEXT_PUBLIC_FONT_SUBSET || 'chinese-simplified',
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
-    // '"LXGW WenKai"',
-    '"PingFang SC"',
+    '"PingFang SC"', // 优先使用苹方字体
     '-apple-system',
     'BlinkMacSystemFont',
-    '"Hiragino Sans GB"',
+    '"LXGW WenKai"',
     '"Microsoft YaHei"',
+    '"Apple Color Emoji"',
     '"Segoe UI Emoji"',
     '"Segoe UI Symbol"',
     '"Segoe UI"',
@@ -36,8 +37,7 @@ module.exports = {
     'Helvetica',
     '"Source Han Sans SC"',
     'Arial',
-    'sans-serif',
-    '"Apple Color Emoji"'
+    'sans-serif'
   ],
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
@@ -54,7 +54,7 @@ module.exports = {
   ],
   FONT_AWESOME:
     process.env.NEXT_PUBLIC_FONT_AWESOME_PATH ||
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
+    'https://sf.akass.cn/font-awesome/6.7.2/css/all.min.css' // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
 
   // END ************网站字体*****************
 }
