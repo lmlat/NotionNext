fb1932be41e3383bb2c701db1bb507f4aa075746
lmlat <38957459+lmlat@users.noreply.github.com>
2026-02-10 23:20:22 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -127,8 +127,6 @@ const nextConfig = {
       'avatars.githubusercontent.com',
       'images.unsplash.com',
       'source.unsplash.com',
-      'cdn.staticaly.com',
-      'https://cdn.jsdmirror.com',
       'p1.qhimg.com',
       'webmention.io',
       'ko-fi.com'
@@ -312,7 +310,9 @@ const nextConfig = {
 
     // Enable source maps in development mode
     if (dev || process.env.NODE_ENV_API === 'development') {
+      // config.devtool = 'source-map'
       config.devtool = 'eval-source-map'
+      // console.log('启动调试 nextjs.config.devtool ', config.devtool)
     }
 
     // 优化模块解析
