e3eda7dccf4e677ec92651d5d64e4abe495e30a9
lmlat <38957459+lmlat@users.noreply.github.com>
2026-02-11 04:36:31 +0800

    Update CategoryBar.js

--- themes/heo/components/CategoryBar.js
+++ themes/heo/components/CategoryBar.js
@@ -74,7 +74,7 @@ const MenuItem = ({ href, name }) => {
   const router = useRouter()
   const { category } = router.query
   const selected = category === name
-  console.log("selected:", selected, "category:", category)
+  console.log("selected:", selected, "category:", category, router.query)
   return (
     <div
       className={`whitespace-nowrap mr-2 duration-200 transition-all font-bold px-2 py-0.5 rounded-md text-gray-900 dark:text-white hover:text-white hover:bg-indigo-600 dark:hover:bg-yellow-600 ${selected ? 'text-white bg-indigo-600 dark:bg-yellow-600' : ''}`}>
