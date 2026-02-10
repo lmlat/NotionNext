66ff39c7a6b7ceae479dc7517e9f6c8fa524e572
lmlat <38957459+lmlat@users.noreply.github.com>
2026-02-11 04:44:37 +0800

    Update CategoryBar.js

--- themes/heo/components/CategoryBar.js
+++ themes/heo/components/CategoryBar.js
@@ -73,8 +73,9 @@ export default function CategoryBar(props) {
 const MenuItem = ({ href, name }) => {
   const router = useRouter()
   const { category } = router.query
-  const selected = category === name
-  console.log("selected:", selected, "category:", category, router.query)
+  const decodedCategory = typeof category === 'string' ? decodeURIComponent(category) : ''
+  const selected = decodedCategory === name
+  console.log("selected:", selected, "category:", category, router.query, decodedCategory)
   return (
     <div
       className={`whitespace-nowrap mr-2 duration-200 transition-all font-bold px-2 py-0.5 rounded-md text-gray-900 dark:text-white hover:text-white hover:bg-indigo-600 dark:hover:bg-yellow-600 ${selected ? 'text-white bg-indigo-600 dark:bg-yellow-600' : ''}`}>
