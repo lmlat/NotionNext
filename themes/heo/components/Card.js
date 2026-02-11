7e48f817e0e98a12d692d3f126ab2b82e9b42e5f
lmlat <38957459+lmlat@users.noreply.github.com>
2026-02-11 17:59:01 +0800

    Update Card.js

--- themes/heo/components/Card.js
+++ themes/heo/components/Card.js
@@ -1,4 +1,5 @@
 const Card = ({ children, headerSlot, className }) => {
+  console.log("Card:", children)
   return <div className={`${className || ''} card border dark:border-gray-700 rounded-xl lg:p-6 p-4`}>
     <>{headerSlot}</>
     <section>
